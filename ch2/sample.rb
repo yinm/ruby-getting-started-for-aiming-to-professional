a17790ccd565885d7801d25037e8201546e48a0b
yinm <yinm.cminor9@gmail.com>
2017-11-20 08:16:24 +0900

    Add rational and complex class

--- ch2/sample.rb
+++ ch2/sample.rb
@@ -1,2 +1,15 @@
-p 10.class
-p 999999999999999999999999999999.class
+r = 2 / 3r
+p r
+p r.class
+
+r = '2/3'.to_r
+p r
+p r.class
+
+c = 0.3 - 0.5i
+p c
+p c.class
+
+c = '0.3-0.5i'.to_c
+p c
+p c.class
