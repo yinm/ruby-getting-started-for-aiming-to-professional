dc2ba97f8d9545457de9d34ce9a9f5c0ed46f410
yinm <yinm.cminor9@gmail.com>
2017-11-19 21:22:14 +0900

    Add method 3

--- ch2/sample.rb
+++ ch2/sample.rb
@@ -1,15 +1,10 @@
-def hello_world
-  'Hello, world!'
+def greeting(country)
+  if country == 'japan'
+    'こんにちは'
+  else
+    'Hello'
+  end
 end
 
-def _hello_world
-  'Hello, world!'
-end
-
-def hello_world_2
-  'Hello, world!!'
-end
-
-puts hello_world
-puts _hello_world
-puts hello_world_2
+puts greeting('japan')
+puts greeting('us')
