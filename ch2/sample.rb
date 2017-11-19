d59fcc13f8a490b717184cbc3573cba324c5412e
yinm <yinm.cminor9@gmail.com>
2017-11-19 21:40:53 +0900

    Add %q literal 2

--- ch2/sample.rb
+++ ch2/sample.rb
@@ -1,7 +1,3 @@
-puts %q!He said, "Don't speak."!
+puts %q?He said, "Don't speak."?
 
-something = "Hello."
-puts %Q!He said, "#{something}"!
-
-something = "Bye."
-puts %!He said, "#{something}"!
+puts %q{He said, "Don't speak."}
