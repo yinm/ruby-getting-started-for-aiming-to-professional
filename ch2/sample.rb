7fec1867dbec7a4c043abdec9574a0a19063bc34
yinm <yinm.cminor9@gmail.com>
2017-11-21 06:33:40 +0900

    Define ? method

--- ch2/sample.rb
+++ ch2/sample.rb
@@ -1,15 +1,7 @@
-p ''.empty?
-p 'abc'.empty?
+def multiple_of_three?(n)
+  n % 3 == 0
+end
 
-p 'watch'.include?('at')
-p 'watch'.include?('in')
-
-p 1.odd?
-p 2.odd?
-
-p 1.even?
-p 2.even?
-
-p nil.nil?
-p 'abc'.nil?
-p 1.nil?
+p multiple_of_three?(4)
+p multiple_of_three?(5)
+p multiple_of_three?(6)
