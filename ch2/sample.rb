dbc14e09921a8e5fb498961519d6d1c2830889fc
yinm <yinm.cminor9@gmail.com>
2017-11-21 06:48:01 +0900

    Check object references 2

--- ch2/sample.rb
+++ ch2/sample.rb
@@ -1,16 +1,12 @@
 a = 'hello'
 b = 'hello'
-
-p a.object_id
-p b.object_id
-
 c = b
-p c.object_id
 
-def m(d)
-  d.object_id
+def m!(d)
+  d.upcase!
 end
-p m(c)
+m!(c)
 
-p a.equal?(b)
-p b.equal?(c)
+p b
+p c
+p a
