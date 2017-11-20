f39d6ab692a3c2ebe8fdf0a9cffcfdd886507b64
yinm <yinm.cminor9@gmail.com>
2017-11-21 06:21:52 +0900

    Add substitution by case statement

--- ch2/sample.rb
+++ ch2/sample.rb
@@ -1,12 +1,15 @@
-country = 'アメリカ'
+country = 'italy'
 
-case country
-  when 'japan', '日本'
-    p 'こんにちは'
-  when 'us', 'アメリカ'
-    p 'Hello'
-  when 'italy', 'イタリア'
-    p 'ciao'
-  else
-    p '????'
-end
+message =
+  case country
+    when 'japan'
+      'こんにちは'
+    when 'us'
+      'Hello'
+    when 'italy'
+      'ciao'
+    else
+      '????'
+  end
+
+p message
