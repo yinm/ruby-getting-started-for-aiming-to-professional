646562ff8cc9d6a589ad4cac5fa7b017de42433c
yinm <yinm.cminor9@gmail.com>
2017-11-21 06:30:33 +0900

    Add default arguments 2

--- ch2/sample.rb
+++ ch2/sample.rb
@@ -1,7 +1,9 @@
-def default_args(a, b, c = 0, d = 0)
-  "a=#{a}, b=#{b}, c=#{c}, d=#{d}"
+def foo(time = Time.now, message = bar)
+  puts "time: #{time}, message: #{message}"
 end
 
-p default_args(1, 2)
-p default_args(1, 2, 3)
-p default_args(1, 2, 3, 4)
+def bar
+  'BAR'
+end
+
+foo
