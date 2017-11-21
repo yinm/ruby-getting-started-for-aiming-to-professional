cf3ead103282bcd889d6c7d17e1a0862e26e7fb7
yinm <yinm.cminor9@gmail.com>
2017-11-21 22:39:26 +0900

    Add range with case statement

--- ch4/sample.rb
+++ ch4/sample.rb
@@ -1,17 +1,17 @@
-def liquid?(temperature)
-  0 <= temperature && temperature < 100
+def charge(age)
+  case age
+    when 0..5
+      0
+    when 6..12
+      300
+    when 13..18
+      600
+    else
+      1000
+  end
 end
-p liquid?(-1)
-p liquid?(0)
-p liquid?(99)
-p liquid?(100)
 
-p '--------------'
-
-def liquid_by_range?(temperature)
-  (0...100).include?(temperature)
-end
-p liquid?(-1)
-p liquid?(0)
-p liquid?(99)
-p liquid?(100)
+p charge(3)
+p charge(12)
+p charge(16)
+p charge(25)
