d47b676c302073b351674804fea7b489f47bf397
yinm <yinm.cminor9@gmail.com>
2017-11-21 22:31:02 +0900

    Check Error

--- ch4/sample.rb
+++ ch4/sample.rb
@@ -1,15 +1,3 @@
-range = 1..5
-p range.include?(0)
-p range.include?(1)
-p range.include?(4.9)
-p range.include?(5)
-p range.include?(6)
+# p 1..5.include?(1)
 
-p '-----'
-
-range = 1...5
-p range.include?(0)
-p range.include?(1)
-p range.include?(4.9)
-p range.include?(5)
-p range.include?(6)
+p (1..5).include?(1)
