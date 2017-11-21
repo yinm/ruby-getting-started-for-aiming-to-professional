43be67a6a8262ba26c2a98979c0c520fcb7ef794
yinm <yinm.cminor9@gmail.com>
2017-11-21 21:53:49 +0900

    Check no line feed

--- ch4/sample.rb
+++ ch4/sample.rb
@@ -1,11 +1,5 @@
 numbers = [1, 2, 3, 4]
 sum = 0
-sum_value = 100
 
-numbers.each do |n|
-  sum_value = n.even? ? n * 10 : n
-  sum += sum_value
-end
-
+numbers.each do |n| sum += n end
 p sum
-p sum_value
