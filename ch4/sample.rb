fbd3369d81f77e6de95edeba43f91a24a61b940d
yinm <yinm.cminor9@gmail.com>
2017-11-21 22:14:59 +0900

    Add (&:) operator

--- ch4/sample.rb
+++ ch4/sample.rb
@@ -1,1 +1,7 @@
-p ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'].inject('Sun') { |result, s| result + s }
+programming_languages = ['ruby', 'java', 'perl']
+p programming_languages.map { |s| s.upcase }
+p programming_languages.map(&:upcase)
+
+numbers = [1, 2, 3, 4, 5, 6]
+p numbers.select { |n| n.odd? }
+p numbers.select(&:odd?)
