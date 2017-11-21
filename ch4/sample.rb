2290d31dd309954d8009d782e1f7e5db0f95e713
yinm <yinm.cminor9@gmail.com>
2017-11-21 22:41:17 +0900

    Add range with array

--- ch4/sample.rb
+++ ch4/sample.rb
@@ -1,17 +1,8 @@
-def charge(age)
-  case age
-    when 0..5
-      0
-    when 6..12
-      300
-    when 13..18
-      600
-    else
-      1000
-  end
-end
+p (1..5).to_a
+p (1...5).to_a
 
-p charge(3)
-p charge(12)
-p charge(16)
-p charge(25)
+p ('a'..'e').to_a
+p ('a'...'e').to_a
+
+p ('bad'..'bag').to_a
+p ('bad'...'bag').to_a
