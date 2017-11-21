d2bb4cea0625e78bfa40838610228bbf761ab17d
yinm <yinm.cminor9@gmail.com>
2017-11-21 22:37:19 +0900

    Add range instead of comparison operators

--- ch4/sample.rb
+++ ch4/sample.rb
@@ -1,2 +1,17 @@
-a = 'abcdef'
-p a[1..3]
+def liquid?(temperature)
+  0 <= temperature && temperature < 100
+end
+p liquid?(-1)
+p liquid?(0)
+p liquid?(99)
+p liquid?(100)
+
+p '--------------'
+
+def liquid_by_range?(temperature)
+  (0...100).include?(temperature)
+end
+p liquid?(-1)
+p liquid?(0)
+p liquid?(99)
+p liquid?(100)
