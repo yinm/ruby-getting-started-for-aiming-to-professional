b1fbe3342b48c9782505e89d4244c1476063c029
yinm <yinm.cminor9@gmail.com>
2017-11-21 21:45:55 +0900

    Check each method 3

--- ch4/sample.rb
+++ ch4/sample.rb
@@ -1,8 +1,17 @@
 numbers = [1, 2, 3, 4]
 sum = 0
 
-numbers.each do |n|
-  sum += n
+numbers.each do |i|
+  sum += i
 end
+p sum
 
+numbers.each do |number|
+  sum += number
+end
+p sum
+
+numbers.each do |element|
+  sum += element
+end
 p sum
