e1f84fadfb7c99d9fb619d48d91d6e4aa6707c74
yinm <yinm.cminor9@gmail.com>
2017-11-21 22:01:56 +0900

    Check reject method

--- ch4/sample.rb
+++ ch4/sample.rb
@@ -1,6 +1,3 @@
 numbers = [1, 2, 3, 4, 5, 6]
-even_numbers = numbers.select { |n| n.even? }
-p even_numbers
-
-odd_numbers = numbers.find_all { |n| n.odd? }
-p odd_numbers
+non_multiples_of_three = numbers.reject { |n| n % 3 == 0 }
+p non_multiples_of_three
