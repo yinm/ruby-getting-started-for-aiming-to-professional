e0d5d67d8b67d8e91833ce63fc42ca08ba6404d3
yinm <yinm.cminor9@gmail.com>
2017-11-21 22:00:34 +0900

    Check select / find_all methods

--- ch4/sample.rb
+++ ch4/sample.rb
@@ -1,3 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-new_numbers = numbers.map { |n| n * 10 }
-p new_numbers
+numbers = [1, 2, 3, 4, 5, 6]
+even_numbers = numbers.select { |n| n.even? }
+p even_numbers
+
+odd_numbers = numbers.find_all { |n| n.odd? }
+p odd_numbers
