840a87f3e1f29c485931b4502a79ba8a4a1887df
yinm <yinm@pepabo.com>
2017-11-21 09:43:05 +0900

    Add tests

--- ruby-book/lib/fizz_buzz.rb
+++ ruby-book/lib/fizz_buzz.rb
@@ -10,6 +10,16 @@ def fizz_buzz(n)
   end
 end
 
+require 'minitest/autorun'
+
+class FizzBuzzTest < Minitest::Test
+  def test_fizz_buzz
+    assert_equal '1', fizz_buzz(1)
+    assert_equal '2', fizz_buzz(2)
+    assert_equal 'Fizz', fizz_buzz(3)
+  end
+end
+
 # puts fizz_buzz(1)
 # puts fizz_buzz(2)
 # puts fizz_buzz(3)
