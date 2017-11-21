8f807001c3c4ac1d86d7bdcd04a96c5db136311f
yinm <yinm.cminor9@gmail.com>
2017-11-21 22:23:28 +0900

    Check range class

--- ch4/sample.rb
+++ ch4/sample.rb
@@ -1,9 +1,2 @@
-p [1, 2, 3, 4, 5, 6].select { |n| n % 3 == 0 }
-
-p [9, 10, 11, 12].map { |n| n.to_s(16) }
-
-a = [1, 2, 3, 4].map do |n|
-  m = n * 4
-  m.to_s
-end
-p a
+p (1..5).class
+p (1...5).class
