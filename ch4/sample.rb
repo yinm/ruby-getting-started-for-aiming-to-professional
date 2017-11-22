eac0c03183f8c4727e5d10536a34f83fcf56037e
yinm <yinm@pepabo.com>
2017-11-22 09:26:25 +0900

    Check block notation

--- ch4/sample.rb
+++ ch4/sample.rb
@@ -1,5 +1,8 @@
-File.open("./sample.txt", "w") do |file|
-  file.puts('1行目のテキストです。')
-  file.puts('2行目のテキストです。')
-  file.puts('3行目のテキストです。')
+a = [1, 2, 3]
+
+p a.delete(100)
+
+a = a.delete(100) do
+  'NG'
 end
+p a
