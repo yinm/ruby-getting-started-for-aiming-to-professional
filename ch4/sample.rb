7dba4bbd4f926cb287ac60914f0e403c0955e5d8
yinm <yinm.cminor9@gmail.com>
2017-11-23 16:41:56 +0900

    Check return value of break

--- ch4/sample.rb
+++ ch4/sample.rb
@@ -1,9 +1,11 @@
-numbers = [1, 2, 3, 4, 5].shuffle
-i = 0
+ret =
+  while true
+    break
+  end
+p ret
 
-while i < numbers.size
-  n = numbers[i]
-  puts n
-  break if n == 5
-  i += 1
-end
+ret =
+  while true
+    break 123
+  end
+p ret
