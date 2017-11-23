b27958095644c80a374c90fad1588f7b2715dc08
yinm <yinm.cminor9@gmail.com>
2017-11-23 17:11:54 +0900

    Check redo 2

--- ch4/sample.rb
+++ ch4/sample.rb
@@ -1,9 +1,13 @@
 foods = ['ピーマン', 'トマト', 'セロリ']
+count = 0
 
 foods.each do |food|
   print "#{food}は好きですか？ => "
-  answer = ['はい', 'いいえ'].sample
+  answer = 'いいえ'
   puts answer
+  count += 1
 
-  redo unless answer == 'はい'
+  redo if answer != 'はい' && count < 2
+
+  count = 0
 end
