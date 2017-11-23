71d8aa12abee5b8f491c94843cc481699df78c16
yinm <yinm.cminor9@gmail.com>
2017-11-24 07:55:49 +0900

    Check keywords arguments 3

--- ch5/sample.rb
+++ ch5/sample.rb
@@ -10,5 +10,6 @@ def buy_burger(menu, drink: true, potato: true)
   end
 end
 
-buy_burger('cheese', drink: true, potato: true)
-buy_burger('fish', drink: true, potato: false)
+buy_burger('cheese')
+buy_burger('fish', potato: false)
+
