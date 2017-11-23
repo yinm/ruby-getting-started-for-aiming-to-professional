ab1a38b8a8649eaeffaa45b85ffdec7e2c0a9295
yinm <yinm.cminor9@gmail.com>
2017-11-23 17:00:16 +0900

    Check catch ~ throw 3

--- ch4/sample.rb
+++ ch4/sample.rb
@@ -1,13 +1,11 @@
-fruits = ['apple', 'melon', 'orange']
-numbers = [1, 2, 3]
-
-catch :done do
-  fruits.shuffle.each do |fruit|
-    numbers.shuffle.each do |n|
-      puts "#{fruit}, #{n}"
-      if fruit == 'orange' && n == 3
-        throw :foo
-      end
-    end
+ret =
+  catch :done do
+    throw :done
   end
-end
+p ret
+
+ret =
+  catch :done do
+    throw :done, 123
+  end
+p ret
