99870703dee742df91994adbce19aeceb38e0a02
yinm <yinm.cminor9@gmail.com>
2017-11-23 17:04:26 +0900

    Check return in loop

--- ch4/sample.rb
+++ ch4/sample.rb
@@ -1,13 +1,13 @@
-def calc_with_break
+def calc_with_return
   numbers = [1, 2, 3, 4, 5, 6]
   target = nil
 
   numbers.shuffle.each do |n|
     target = n
-    break if n.even?
+    return if n.even?
   end
 
   target * 10
 end
 
-p calc_with_break
+p calc_with_return
