65fbfcc8fcd734c45e4f96bccf20d521a98f9efb
yinm <yinm.cminor9@gmail.com>
2017-11-25 08:07:17 +0900

    Check shortcut hash in last argument

--- ch5/sample.rb
+++ ch5/sample.rb
@@ -1,5 +1,6 @@
-def buy_burger(menu, drink: true, potato: true, **others)
-  puts others
+def buy_burger(menu, options = {})
+  puts options
 end
 
-buy_burger('fish', drink: true, potato: false, salad: true, chicken: false)
+buy_burger('fish', {'drink' => true, 'potato' => false})
+buy_burger('fish', 'drink' => true, 'potato' => false)
