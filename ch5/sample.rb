05ef7615f25bdce2eeafc82752748e0096fa492c
yinm <yinm.cminor9@gmail.com>
2017-11-25 08:13:40 +0900

    Check shortcut () in argument

--- ch5/sample.rb
+++ ch5/sample.rb
@@ -2,5 +2,5 @@ def buy_burger(options = {}, menu)
   puts options
 end
 
-# buy_burger('drink' => true, 'potato' => false, 'fish')
+# buy_burger {'drink' => true, 'potato' => false}, 'fish'
 buy_burger({'drink' => true, 'potato' => false}, 'fish')
