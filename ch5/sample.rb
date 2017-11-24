043e6f16b05fb11f5271820eadb45d2f7b54a9d1
yinm <yinm.cminor9@gmail.com>
2017-11-25 08:09:00 +0900

    Check hash in argument

--- ch5/sample.rb
+++ ch5/sample.rb
@@ -1,6 +1,6 @@
-def buy_burger(menu, options = {})
+def buy_burger(options = {}, menu)
   puts options
 end
 
-buy_burger('fish', {'drink' => true, 'potato' => false})
-buy_burger('fish', 'drink' => true, 'potato' => false)
+# buy_burger('drink' => true, 'potato' => false, 'fish')
+buy_burger({'drink' => true, 'potato' => false}, 'fish')
