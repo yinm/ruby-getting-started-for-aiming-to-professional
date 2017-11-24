ec7c899b0e96ae957230ad039b057a24b0e8e312
yinm <yinm.cminor9@gmail.com>
2017-11-25 08:59:50 +0900

    Check idiom

--- ch5/sample.rb
+++ ch5/sample.rb
@@ -1,2 +1,15 @@
-p 'apple'.include?('pp')
-p 'apple'.include?(:pp)
+def find_currency(country)
+  currencies = { japan: 'yen', us: 'dollar', india: 'rupee' }
+  currencies[country]
+end
+
+def show_currency(country)
+  currency = find_currency(country)
+
+  if currency
+    currency.upcase
+  end
+end
+
+p show_currency(:japan)
+p show_currency(:brazil)
