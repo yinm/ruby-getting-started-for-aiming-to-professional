1c80c75f76cc78786fd3a694b993fe6729d4bb9d
yinm <yinm@pepabo.com>
2017-11-24 09:55:44 +0900

    Check pseudo keywords arguments

--- ch5/sample.rb
+++ ch5/sample.rb
@@ -1,3 +1,16 @@
-h = { us: 'dollar', india: 'rupee' }
-merged_h = { japan: 'yen' }.merge(h)
-p merged_h
+def buy_burger(menu, options = {})
+  drink = options[:drink]
+  potato = options[:potato]
+
+  puts "buy #{menu} burger"
+
+  if drink
+    puts 'with drink'
+  end
+
+  if potato
+    puts 'with potato'
+  end
+end
+
+buy_burger('cheese', drink: true, potato: true)
