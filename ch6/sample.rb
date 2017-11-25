f0a20b9db5d62ad3a35e88db41bc80cc90361e92
yinm <yinm.cminor9@gmail.com>
2017-11-25 15:29:35 +0900

    Check Regexp::EXTENDED

--- ch6/sample.rb
+++ ch6/sample.rb
@@ -1,7 +1,8 @@
-regexp = /
-  \d{3}
-  \  #半角スペースで区切る
-  \d {4}
-/x
+pattern = <<'TEXT'
+  \d{3} # 郵便番号の先頭3桁
+  -     # 区切り文字のハイフン
+  \d{4} # 郵便番号の末尾4桁
+TEXT
 
-p '123 4567' =~ regexp
+regexp = Regexp.new(pattern, Regexp::EXTENDED)
+p '123-4567' =~ regexp
