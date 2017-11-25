aa52e953ca7c903e34fc93132d0adb35090b01a0
yinm <yinm.cminor9@gmail.com>
2017-11-25 10:53:56 +0900

    Check meta character of ?<name>

--- ch6/sample.rb
+++ ch6/sample.rb
@@ -1,3 +1,10 @@
 text = '私の誕生日は1977年7月17日です。'
-m = text.match(/(\d+)年(\d+)月(\d+)日/)
-p m
+m = /(?<year>\d+)年(?<month>\d+)月(?<day>\d+)日/.match(text)
+
+p m[:year]
+p m[:month]
+p m[:day]
+
+p m['year']
+
+p m[2]
