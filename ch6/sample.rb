4c6bc5eed407847f53dc0747978cf4189ecdd26b
yinm <yinm.cminor9@gmail.com>
2017-11-25 14:53:14 +0900

    Check #{} with regexp

--- ch6/sample.rb
+++ ch6/sample.rb
@@ -1,3 +1,4 @@
-p /http:\/\/example\.com/
-p %r!http://example\.com!
-p %r{http://example\.com}
+pattern = '\d{3}-\d{4}'
+p '123-4567' =~ /#{pattern}/
+p '123-4567' =~ %r(#{pattern})
+p '123-4567' =~ /\d{3}-\d{4}/
