c4c193d27a3edf9277f3faa5901013580164076a
yinm <yinm.cminor9@gmail.com>
2017-11-25 15:33:30 +0900

    Check match? method

--- ch6/sample.rb
+++ ch6/sample.rb
@@ -1,2 +1,6 @@
-regexp = Regexp.new('Hello.Bye', Regexp::IGNORECASE | Regexp::MULTILINE)
-p "HELLO\nBYE" =~ regexp
+p /\d{3}-\d{4}/.match?('123-4567')
+
+p $~
+p Regexp.last_match
+
+p '123-4567'.match?(/\d{3}-\d{4}/)
