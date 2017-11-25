c488dd23280efa1284fd7a7bde7a90d54bd45d83
yinm <yinm.cminor9@gmail.com>
2017-11-25 12:03:13 +0900

    Check String.gsub with hash

--- ch6/sample.rb
+++ ch6/sample.rb
@@ -1,6 +1,4 @@
-text = '誕生日は1977年7月17日です'
+text = '123,456-789'
+hash = { ',' => ':', '-' => '/' }
 
-p text.gsub(
-  /(?<year>\d+)年(?<month>\d+)月(?<day>\d+)日/,
-  '\k<year>-\k<month>-\k<day>'
-)
+p text.gsub(/,|-/, hash)
