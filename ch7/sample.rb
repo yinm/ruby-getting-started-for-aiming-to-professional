aab2f64af08f0b695ca625163e80ca573b4b2304
yinm <yinm.cminor9@gmail.com>
2017-11-25 15:38:23 +0900

    Check program of treating user data

---
+++ ch7/sample.rb
@@ -0,0 +1,7 @@
+users = []
+users << { first_name: 'Alice', last_name: 'Ruby', age: 20 }
+users << { first_name: 'Bob', last_name: 'Python', age: 30 }
+
+users.each do |user|
+  puts "氏名: #{user[:first_name]} #{user[:last_name]}、 年齢: #{user[:age]}"
+end
