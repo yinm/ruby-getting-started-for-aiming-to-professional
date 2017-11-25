2ef874fe8b60907b629c79b6462f7bb0e617a84d
yinm <yinm.cminor9@gmail.com>
2017-11-25 11:20:05 +0900

    Check [] with regexp 4

--- ch6/sample.rb
+++ ch6/sample.rb
@@ -1,4 +1,4 @@
 text = '誕生日は1977年7月17日です。'
 
-p text[/(\d+)年(\d+)月(\d+)日/]
-p text[/(\d+)年(\d+)月(\d+)日/, 3]
+p text[/(?<year>\d+)年(?<month>\d+)月(?<day>\d+)日/, :day]
+p text[/(?<year>\d+)年(?<month>\d+)月(?<day>\d+)日/, 'day']
