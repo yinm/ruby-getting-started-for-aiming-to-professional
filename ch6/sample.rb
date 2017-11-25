c912f24a872d1789e6177e55d662707e84a6c3d7
yinm <yinm.cminor9@gmail.com>
2017-11-25 15:30:44 +0900

    Check multiple options

--- ch6/sample.rb
+++ ch6/sample.rb
@@ -1,8 +1,1 @@
-pattern = <<'TEXT'
-  \d{3} # 郵便番号の先頭3桁
-  -     # 区切り文字のハイフン
-  \d{4} # 郵便番号の末尾4桁
-TEXT
-
-regexp = Regexp.new(pattern, Regexp::EXTENDED)
-p '123-4567' =~ regexp
+p "HELLO\nBYE" =~ /Hello.Bye/im
