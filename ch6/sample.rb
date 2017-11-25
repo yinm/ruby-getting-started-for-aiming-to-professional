b63832aa6fabad59ffbe9fe36bbd4690273c9f94
yinm <yinm.cminor9@gmail.com>
2017-11-25 10:49:25 +0900

    Check MatchData object

--- ch6/sample.rb
+++ ch6/sample.rb
@@ -1,7 +1,8 @@
 text = '私の誕生日は1977年7月17日です。'
+m = /(\d+)年(\d+)月(\d+)日/.match(text)
 
-if m = /(\d+)年(\d+)月(\d+)日/.match(text)
-  p m
-else
-  puts 'unmatch'
-end
+p m[0]
+p m[1]
+p m[2, 2]
+p m[-1]
+p m[1..3]
