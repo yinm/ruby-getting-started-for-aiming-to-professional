968aae22fe96dec00a40540a76eaab165daa50fb
yinm <yinm.cminor9@gmail.com>
2017-11-25 11:39:34 +0900

    Check String.slice method

--- ch6/sample.rb
+++ ch6/sample.rb
@@ -1,4 +1,7 @@
-text = '誕生日は1977年7月17日です。'
+text = '郵便番号は123-4567です'
+p text.slice(/\d{3}-\d{4}/)
 
-p text[/(?<year>\d+)年(?<month>\d+)月(?<day>\d+)日/, :day]
-p text[/(?<year>\d+)年(?<month>\d+)月(?<day>\d+)日/, 'day']
+text = '誕生日は1977年7月17日です'
+p text.slice(/(\d+)年(\d+)月(\d+)日/, 3)
+p text.slice(/(?<year>\d+)年(?<month>\d+)月(?<day>\d+)日/)
+p text.slice(/(?<year>\d+)年(?<month>\d+)月(?<day>\d+)日/, :year)
