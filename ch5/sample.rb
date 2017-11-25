56784235d78286b0c15b9fcd2420ca74201ab794
yinm <yinm.cminor9@gmail.com>
2017-11-25 09:01:37 +0900

    Check lonely operator

--- ch5/sample.rb
+++ ch5/sample.rb
@@ -1,13 +1,5 @@
-def find_currency(country)
-  currencies = { japan: 'yen', us: 'dollar', india: 'rupee' }
-  currencies[country]
-end
+a = 'foo'
+p a&.upcase
 
-def show_currency(country)
-  if currency = find_currency(country)
-    currency.upcase
-  end
-end
-
-p show_currency(:japan)
-p show_currency(:brazil)
+a = nil
+p a&.upcase
