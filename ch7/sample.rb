94f53feb5a86c316c22516613b91385f52bcd732
yinm <yinm.cminor9@gmail.com>
2017-11-27 08:00:21 +0900

    Check not defining instance variable

--- ch7/sample.rb
+++ ch7/sample.rb
@@ -1,11 +1,10 @@
 class User
   def initialize(name)
-    @name = name
+    # @name = name
   end
 
   def hello
-    # shuffled_name = @name.chars.shuffle.join
-    "Hello, I am #{shuffled_name}."
+    "Hello, I am #{@name}."
   end
 end
 
