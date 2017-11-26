12e1dde47a70999e8913e6604b265958c8234725
yinm <yinm.cminor9@gmail.com>
2017-11-27 07:31:34 +0900

    Define class

--- ch7/sample.rb
+++ ch7/sample.rb
@@ -1,11 +1,21 @@
+class User
+  attr_reader :first_name, :last_name, :age
+
+  def initialize(first_name, last_name, age)
+    @first_name = first_name
+    @last_name = last_name
+    @age = age
+  end
+end
+
 users = []
-users << { first_name: 'Alice', last_name: 'Ruby', age: 20 }
-users << { first_name: 'Bob', last_name: 'Python', age: 30 }
+users << User.new('Alice', 'Ruby', 20)
+users << User.new('Bob', 'Python', 30)
 
 def full_name(user)
-  "#{user[:first_name]} #{user[:last_name]}"
+  "#{user.first_name} #{user.last_name}"
 end
 
 users.each do |user|
-  puts "氏名: #{full_name(user)}, 年齢: #{user[:age]}"
+  puts "氏名: #{full_name(user)}、 年齢: #{user.age}"
 end
