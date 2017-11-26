e997e4f4b697094b44a8efa8ea372e82ef1c3527
yinm <yinm.cminor9@gmail.com>
2017-11-26 19:34:36 +0900

    Add method of return full name

--- ch7/sample.rb
+++ ch7/sample.rb
@@ -2,6 +2,10 @@ users = []
 users << { first_name: 'Alice', last_name: 'Ruby', age: 20 }
 users << { first_name: 'Bob', last_name: 'Python', age: 30 }
 
+def full_name(user)
+  "#{user[:first_name]} #{user[:last_name]}"
+end
+
 users.each do |user|
-  puts "氏名: #{user[:first_name]} #{user[:last_name]}、 年齢: #{user[:age]}"
+  puts "氏名: #{full_name(user)}, 年齢: #{user[:age]}"
 end
