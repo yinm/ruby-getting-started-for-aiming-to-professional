e6e3c3a88d61ff90b6736e8efcd7e420f9cae43b
yinm <yinm.cminor9@gmail.com>
2017-11-27 07:36:56 +0900

    Define method

--- ch7/sample.rb
+++ ch7/sample.rb
@@ -6,22 +6,16 @@ class User
     @last_name = last_name
     @age = age
   end
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
 
 users = []
 users << User.new('Alice', 'Ruby', 20)
 users << User.new('Bob', 'Python', 30)
 
-def full_name(user)
-  "#{user.first_name} #{user.last_name}"
-end
-
 users.each do |user|
-  puts "氏名: #{full_name(user)}、 年齢: #{user.age}"
+  puts "氏名: #{user.full_name}, 年齢: #{user.age}"
 end
-
-p users[0].first_name
-# p users[0].first_mame
-
-# users[0].country = 'japan'
-# users[0].first_name = 'Carol'
