784ea9d71f73d90ce8738b7b65c51dd361e7bee4
yinm <yinm.cminor9@gmail.com>
2017-11-28 07:52:38 +0900

    Check class method

--- ch7/sample.rb
+++ ch7/sample.rb
@@ -3,13 +3,19 @@ class User
     @name = name
   end
 
+  def self.create_users(names)
+    names.map do |name|
+      User.new(name)
+    end
+  end
+
   def hello
     "Hello, I am #{@name}"
   end
 end
 
-alice = User.new('Alice')
-puts alice.hello
-
-bob = User.new('Bob')
-puts bob.hello
+names = ['Alice', 'Bob', 'Carol']
+users = User.create_users(names)
+users.each do |user|
+  puts user.hello
+end
