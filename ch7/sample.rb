cb1deb51e70fe399ad4b476ee567d945244ee655
yinm <yinm.cminor9@gmail.com>
2017-11-28 07:58:14 +0900

    Check class constant with class method and instance method

--- ch7/sample.rb
+++ ch7/sample.rb
@@ -1,13 +1,16 @@
 class Product
   DEFAULT_PRICE = 0
 
-  attr_reader :name, :price
+  def self.default_price
+    DEFAULT_PRICE
+  end
 
-  def initialize(name, price = DEFAULT_PRICE)
-    @name = name
-    @price = price
+  def default_price
+    DEFAULT_PRICE
   end
 end
 
-product = Product.new('A free movie')
-p product.price
+p Product.default_price
+
+product = Product.new
+p product.default_price
