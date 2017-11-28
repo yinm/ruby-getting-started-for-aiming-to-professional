729f77ac8ac814950933f1c7ae0ef2cbfcc86770
yinm <yinm.cminor9@gmail.com>
2017-11-29 08:08:01 +0900

    Check calling class method in instance method

--- ch7/sample.rb
+++ ch7/sample.rb
@@ -1,5 +1,20 @@
-class Foo
-  3.times do
-    puts 'hello!'
+class Product
+  attr_reader :name, :price
+
+  def initialize(name, price)
+    @name = name
+    @price = price
+  end
+
+  def self.format_price(price)
+    "#{price}円"
+  end
+
+  def to_s
+    formatted_price = Product.format_price(price)
+    "name: #{name}, price: #{formatted_price}"
   end
 end
+
+product = Product.new('A great movie', 1000)
+p product.to_s
