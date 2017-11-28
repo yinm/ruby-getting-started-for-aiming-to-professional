afa6ed8078d7520ff5122b6c09117c51745c51d7
yinm <yinm.cminor9@gmail.com>
2017-11-29 08:33:56 +0900

    Check is_a? method

--- ch7/sample.rb
+++ ch7/sample.rb
@@ -4,4 +4,10 @@ end
 user = User.new
 
 p user.instance_of?(User)
-p user.instance_of?(String)
+p user.instance_of?(Object)
+
+p user.is_a?(User)
+p user.is_a?(Object)
+p user.is_a?(BasicObject)
+
+p user.is_a?(String)
