0fd1a58d936beb1f54358fcc56e01a132ee6b7ca
yinm <yinm.cminor9@gmail.com>
2017-11-29 08:26:36 +0900

    Check Object class

--- ch7/sample.rb
+++ ch7/sample.rb
@@ -1,21 +1,6 @@
-class Product
-  attr_reader :name, :price
-
-  def initialize(name, price)
-    @name = name
-    @price = price
-  end
-
-  def self.format_price(price)
-    "#{price}円"
-  end
-
-  def to_s
-    # 引数のprice は、 attr_reader で getter を設定しているから使える
-    formatted_price = self.class.format_price(price)
-    "name: #{name}, price: #{formatted_price}"
-  end
+class User
 end
 
-product = Product.new('A great movie', 1000)
-p product.to_s
+user = User.new
+p user.to_s
+p user.nil?
