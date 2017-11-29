12a816f11c097ebc9b87bd0e013280ba52c06a61
yinm <yinm.cminor9@gmail.com>
2017-11-30 08:05:59 +0900

    Check super method 2

--- ch7/sample.rb
+++ ch7/sample.rb
@@ -12,15 +12,12 @@ p product.name
 p product.price
 
 class DVD < Product
-  attr_reader :running_time
-
-  def initialize(name, price, running_time)
-    super(name, price)
-    @running_time = running_time
+  def initialize(name, price)
+    super
   end
 end
 
-dvd = DVD.new('A great movie', 1000, 120)
+dvd = DVD.new('A great movie', 1000)
 p dvd.name
 p dvd.price
-p dvd.running_time
+
