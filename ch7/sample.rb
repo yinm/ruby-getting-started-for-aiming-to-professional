2d02070808066599710769a3ecbba1d6cdfe24dc
yinm <yinm.cminor9@gmail.com>
2017-11-30 08:01:38 +0900

    Check inherit

--- ch7/sample.rb
+++ ch7/sample.rb
@@ -1,9 +1,27 @@
 class Product
+  attr_reader :name, :price
+
+  def initialize(name, price)
+    @name = name
+    @price = price
+  end
 end
+
+product = Product.new('A great movie', 1000)
+p product.name
+p product.price
 
 class DVD < Product
+  attr_reader :running_time
+
+  def initialize(name, price, running_time)
+    @name = name
+    @price = price
+    @running_time = running_time
+  end
 end
 
-dvd = DVD.new
-p dvd.instance_of?(DVD)
-p dvd.is_a?(Product)
+dvd = DVD.new('A great movie', 1000, 120)
+p dvd.name
+p dvd.price
+p dvd.running_time
