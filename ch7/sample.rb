de1eb216b0b9df767b1f18625e6a52076c3b92aa
yinm <yinm.cminor9@gmail.com>
2017-12-01 08:17:29 +0900

    Check class instance method with inherit

--- ch7/sample.rb
+++ ch7/sample.rb
@@ -18,8 +18,27 @@ class Product
   end
 end
 
+class DVD < Product
+  @name = 'DVD'
+
+  def self.name
+    @name
+  end
+
+  def upcase_name
+    @name.upcase
+  end
+end
+
+p Product.name
+p DVD.name
 
 product = Product.new('A great movie')
 p product.name
 
+dvd = DVD.new('An awesome film')
+p dvd.name
+p dvd.upcase_name
+
 p Product.name
+p DVD.name
