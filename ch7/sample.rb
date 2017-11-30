4723e0221cba53052b238a661ae01b05dd7d7881
yinm <yinm.cminor9@gmail.com>
2017-11-30 23:10:00 +0900

    Check freeze 2

--- ch7/sample.rb
+++ ch7/sample.rb
@@ -1,12 +1,6 @@
 class Product
   DEFAULT_PRICE = 0
+
+  freeze
   DEFAULT_PRICE = 1000
 end
-
-p Product::DEFAULT_PRICE
-
-Product::DEFAULT_PRICE = 3000
-p Product::DEFAULT_PRICE
-
-Product.freeze
-Product::DEFAULT_PRICE = 5000
