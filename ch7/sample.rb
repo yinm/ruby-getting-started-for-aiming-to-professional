22fcb7e35f3cd9e697881eb114ae3782f4623f6a
yinm <yinm.cminor9@gmail.com>
2017-11-30 23:03:35 +0900

    Check class constant

--- ch7/sample.rb
+++ ch7/sample.rb
@@ -1,32 +1,5 @@
-class Parent
-  def initialize
-    @first = 1
-    @second = 2
-    @third = 3
-  end
-
-  def number
-    "#{@first}.#{@second}.#{@third}"
-  end
+class Product
+  DEFAULT_PRICE = 0
 end
 
-class Child < Parent
-  def initialize
-    super
-    @hour = 6
-    @minute = 30
-    @second = 59
-  end
-
-  def time
-    "#{@hour}:#{@minute}:#{@second}"
-  end
-end
-
-parent = Parent.new
-p parent.number
-
-child = Child.new
-p child.time
-
-p child.number
+p Product::DEFAULT_PRICE
