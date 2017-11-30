84cbee1c4ee4cfcc94f824b27f1b9f145ebf1e0b
yinm <yinm.cminor9@gmail.com>
2017-12-01 08:35:21 +0900

    Check setter method

--- ch7/sample.rb
+++ ch7/sample.rb
@@ -1,12 +1,15 @@
 class User
-  class BloodType
-    attr_reader :type
-    
-    def initialize(type)
-      @type = type
-    end
+  def name
+    @name
+  end
+  
+  def name=(value)
+    @name = value
   end
 end
 
-blood_type = User::BloodType.new('B')
-p blood_type.type
+user = User.new
+p user.name
+
+user.name = 'Alice'
+p user.name
