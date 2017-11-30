d33e6f0ff6b6a5e6dae34236ff5ecad49e4fa035
yinm <yinm.cminor9@gmail.com>
2017-11-30 23:21:36 +0900

    Check mutable constant with freeze 2

--- ch7/sample.rb
+++ ch7/sample.rb
@@ -1,10 +1,6 @@
 class Product
   SOME_NAMES = ['Foo', 'Bar', 'Baz'].freeze
-
-  def self.names_without_foo(names = SOME_NAMES)
-    names.delete('Foo')
-    names
-  end
 end
 
-Product.names_without_foo
+Product::SOME_NAMES[0].upcase!
+p Product::SOME_NAMES
