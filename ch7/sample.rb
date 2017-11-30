6b6f28b825ac51e6f6991c223408c96c8d2ec995
yinm <yinm.cminor9@gmail.com>
2017-11-30 23:07:45 +0900

    Check re-substitution of constant

--- ch7/sample.rb
+++ ch7/sample.rb
@@ -1,7 +1,9 @@
 class Product
   DEFAULT_PRICE = 0
-
-  private_constant :DEFAULT_PRICE
+  DEFAULT_PRICE = 1000
 end
 
+p Product::DEFAULT_PRICE
+
+Product::DEFAULT_PRICE = 3000
 p Product::DEFAULT_PRICE
