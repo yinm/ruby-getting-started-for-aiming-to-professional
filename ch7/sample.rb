fc0d1f008e63ca7e6f5839301d0e767f2c781b28
yinm <yinm.cminor9@gmail.com>
2017-11-30 23:16:39 +0900

    Check mutable constant

--- ch7/sample.rb
+++ ch7/sample.rb
@@ -1,6 +1,14 @@
 class Product
-  DEFAULT_PRICE = 0
-
-  freeze
-  DEFAULT_PRICE = 1000
+  NAME = 'A product'
+  SOME_NAMES = ['Foo', 'Bar', 'Baz']
+  SOME_PRICES = { 'Foo' => 1000, 'Bar' => 2000, 'Baz' => 3000 }
 end
+
+Product::NAME.upcase!
+p Product::NAME
+
+Product::SOME_NAMES << 'Hoge'
+p Product::SOME_NAMES
+
+Product::SOME_PRICES['Hoge'] = 4000
+p Product::SOME_PRICES
