cae5286524ba00fb59e76665a7e71b523ad235ba
yinm <yinm.cminor9@gmail.com>
2017-12-02 20:42:46 +0900

    Check ===

--- ch7/sample.rb
+++ ch7/sample.rb
@@ -1,10 +1,10 @@
-text = '03-1234-5678'
+value = [1, 2, 3]
 
-case text
-  when /^\d{3}-\d{4}$/
-    puts '郵便番号です'
-  when /^\d{4}\/\d{1,2}\/\d{1,2}$/
-    puts '日付です'
-  when /^\d+-\d+-\d+$/
-    puts '電話番号です'
+case value
+  when String
+    puts '文字列です'
+  when Array
+    puts '配列です'
+  when Hash
+    puts 'ハッシュです'
 end
