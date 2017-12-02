ba07da10caf1334e5d9503890b6107dcad62218f
yinm <yinm.cminor9@gmail.com>
2017-12-02 20:33:54 +0900

    Check equal? method

--- ch7/sample.rb
+++ ch7/sample.rb
@@ -1,30 +1,6 @@
-class Product
-  attr_reader :code, :name
-  
-  def initialize(code, name)
-    @code = code
-    @name = name
-  end
-  
-  def ==(other)
-    if other.is_a?(Product)
-      code == other.code
-    else
-      false
-    end
-  end
-end
+a = 'abc'
+b = 'abc'
+p a.equal?(b)
 
-a = Product.new('A-0001', 'A great movie')
-b = Product.new('B-0001', 'An awesome film')
-c = Product.new('A-0001', 'A great movie')
-
-p a == b
-p a == c
-p a == a
-
-p a == 1
-p a == 'a'
-
-p a.==(b)
-p a.==(c)
+c = a
+p a.equal?(c)
