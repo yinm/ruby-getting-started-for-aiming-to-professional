5bdc7855f797bdd332cdd039baf8ed3593b2ccc9
yinm <yinm.cminor9@gmail.com>
2017-12-04 07:56:28 +0900

    Check class method and singleton method

--- ch7/sample.rb
+++ ch7/sample.rb
@@ -1,9 +1,29 @@
-alice = 'I am Alice.'
+class User
+  def self.hello
+    'Hello.'
+  end
 
-class << alice
-  def shuffle
-    chars.shuffle.join
+  class << self
+    def hi
+      'Hi.'
+    end
   end
 end
 
-p alice.shuffle
+alice = 'I am alice.'
+
+def alice.hello
+  'Hello.'
+end
+
+class << alice
+  def hi
+    'Hi.'
+  end
+end
+
+p User.hello
+p User.hi
+
+p alice.hello
+p alice.hi
