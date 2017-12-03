f6e9f0da74b2eed70614d14303e4046f9367cbb9
yinm <yinm.cminor9@gmail.com>
2017-12-04 08:16:33 +0900

    Check respond_to? 2

--- ch7/sample.rb
+++ ch7/sample.rb
@@ -1,4 +1,10 @@
-s = 'Alice'
+def display_name(object)
+  if object.respond_to?(:name)
+    puts "Name is <<#{object.name}>>"
+  else
+    puts "No name."
+  end
+end
 
-p s.respond_to?(:split)
-p s.respond_to?(:name)
+s = 'Alice'
+display_name(s)
