07d75fdbeec84b2a06268761469fafb9277c7142
yinm <yinm.cminor9@gmail.com>
2017-12-04 08:39:53 +0900

    Check mixin

--- ch8/sample.rb
+++ ch8/sample.rb
@@ -1,26 +1,24 @@
-class Product
-  def title
-    log 'title is called.'
-    'A great movie'
-  end
-
-  private
-
+module Loggable
   def log(text)
     puts "[LOG] #{text}"
   end
 end
 
+class Product
+  include Loggable
+
+  def title
+    log 'title is called.'
+    'A great movie'
+  end
+end
+
 class User
+  include Loggable
+
   def name
     log 'name is called.'
     'Alice'
-  end
-
-  private
-
-  def log(text)
-    puts "[LOG] #{text}"
   end
 end
 
