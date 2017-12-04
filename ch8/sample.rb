0ddb22b382348f9e9cc2d4cb60fd5b6ddc204c29
yinm <yinm.cminor9@gmail.com>
2017-12-05 07:58:42 +0900

    Check extend

--- ch8/sample.rb
+++ ch8/sample.rb
@@ -1,33 +1,16 @@
 module Loggable
-  private
-
   def log(text)
     puts "[LOG] #{text}"
   end
 end
 
 class Product
-  include Loggable
+  extend Loggable
 
-  def title
-    log 'title is called.'
-    'A great movie'
+  def self.create_products(names)
+    log 'create_products is called.'
   end
 end
 
-class User
-  include Loggable
-
-  def name
-    log 'name is called.'
-    'Alice'
-  end
-end
-
-product = Product.new
-p product.title
-
-user = User.new
-p user.name
-
-product.log 'public?'
+Product.create_products([])
+Product.log('Hello.')
