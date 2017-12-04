f134d3f66ecaec26fadd9e91a7be756e77629aaa
yinm <yinm.cminor9@gmail.com>
2017-12-05 08:43:29 +0900

    Check Enumerable

--- ch8/sample.rb
+++ ch8/sample.rb
@@ -1,3 +1,9 @@
-p Array.include?(Enumerable)
-p Hash.include?(Enumerable)
-p Range.include?(Enumerable)
+p [1, 2, 3].map { |n| n * 10 }
+foo = { a: 1, b: 2, c: 3 }.map { |k, v| [k, v * 10] }
+p foo
+p (1..3).map { |n| n * 10 }
+
+p [1, 2, 3].count
+foo = { a: 1, b: 2, c: 3 }.count
+p foo
+p (1..3).count
