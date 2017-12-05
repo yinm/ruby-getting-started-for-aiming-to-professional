2df1e68d0667e22ff38bcc4c1d150ae737662d3c
yinm <yinm.cminor9@gmail.com>
2017-12-06 08:00:01 +0900

    Check comparable module

--- ch8/sample.rb
+++ ch8/sample.rb
@@ -1,7 +1,28 @@
-p 2 > 1
-p 2 <= 1
-p 2 == 1
+class Tempo
+  include Comparable
 
-p 'abc' > 'xyz'
-p 'abc' <= 'xyz'
-p 'abc' == 'xyz'
+  attr_reader :bpm
+
+  def initialize(bpm)
+    @bpm = bpm
+  end
+
+  def <=>(other)
+    if other.is_a?(Tempo)
+      bpm <=> other.bpm
+    else
+      nil
+    end
+  end
+
+  def inspect
+    "#{bpm}bpm"
+  end
+end
+
+p t_120 = Tempo.new(120)
+p t_180 = Tempo.new(180)
+
+p t_120 > t_180
+p t_120 <= t_180
+p t_120 == t_180
