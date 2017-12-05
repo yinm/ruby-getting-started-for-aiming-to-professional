adebd5847d8894d76b9001f8a68ea03c7a861697
yinm <yinm.cminor9@gmail.com>
2017-12-06 08:30:41 +0900

    Check instance variable in module

--- ch8/sample.rb
+++ ch8/sample.rb
@@ -1,9 +1,21 @@
-class User
-  p self
-  p self.class
+module NameChanger
+  def change_name
+    @name = 'ありす'
+  end
 end
 
-module Loggable
-  p self
-  p self.class
+class User
+  include NameChanger
+
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
 end
+
+user = User.new('alice')
+p user.name
+
+user.change_name
+p user.name
