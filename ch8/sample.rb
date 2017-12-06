3b403ee64782c29bd1f80e9507b93727b9e9d75d
yinm <yinm.cminor9@gmail.com>
2017-12-07 07:57:09 +0900

    Check module_function method

--- ch8/sample.rb
+++ ch8/sample.rb
@@ -1,7 +1,21 @@
 module Loggable
-  def self.log(text)
+  def log(text)
     puts "[LOG] #{text}"
   end
+
+  module_function :log
 end
 
 Loggable.log('Hello.')
+
+class Product
+  include Loggable
+
+  def title
+    log 'title is called.'
+    'A great movie'
+  end
+end
+
+product = Product.new
+p product.title
