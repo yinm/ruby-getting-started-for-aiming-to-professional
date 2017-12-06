0bc3e4be4e56523b3bb4ff985e215381c96b336c
yinm <yinm.cminor9@gmail.com>
2017-12-07 08:39:19 +0900

    Check open class

--- ch8/sample.rb
+++ ch8/sample.rb
@@ -1,29 +1,18 @@
-module A
-  def to_s
-    "<A> #{super}"
-  end
-end
-
 class Product
-  prepend A
-
-  def to_s
-    "<Product> #{super}"
-  end
-end
-
-class ProductIncluded
-  include A
-
-  def to_s
-    "<ProductIncluded> #{super}"
+  def name
+    'A great file'
   end
 end
 
 product = Product.new
-p product.to_s
-p Product.ancestors
+p product.name
 
-product_included = ProductIncluded.new
-p product_included.to_s
-p ProductIncluded.ancestors
+class Product
+  alias name_original name
+
+  def name
+    "<<#{name_original}>>"
+  end
+end
+
+p product.name
