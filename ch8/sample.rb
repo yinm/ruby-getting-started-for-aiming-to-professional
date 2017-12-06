1a31120525ae94bbbf3636b0c86ffece558ab3cb
yinm <yinm.cminor9@gmail.com>
2017-12-07 08:02:55 +0900

    Check module_function method 2

--- ch8/sample.rb
+++ ch8/sample.rb
@@ -1,19 +1,31 @@
 module Loggable
+  def bar(text)
+    puts "[BAR] #{text}"
+  end
+
+  module_function
+
   def log(text)
     puts "[LOG] #{text}"
   end
 
-  module_function :log
+  def foo(text)
+    puts "[FOO] #{text}"
+  end
 end
 
-Loggable.log('Hello.')
+Loggable.log 'hello.'
+Loggable.foo 'hello.'
 
 class Product
   include Loggable
 
   def title
-    log 'title is called.'
-    'A great movie'
+    bar 'hello.'
+    log 'hello.'
+    foo 'hello.'
+
+    'testだよ'
   end
 end
 
