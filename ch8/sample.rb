edc693843620ac8966e9a1e2a24d08bceda0fa65
yinm <yinm.cminor9@gmail.com>
2017-12-07 07:40:52 +0900

    Check not nested namespace

--- ch8/sample.rb
+++ ch8/sample.rb
@@ -1,19 +1,11 @@
 module Baseball
-  class Second
-    def initialize(player, uniform_number)
-      @player = player
-      @uniform_number = uniform_number
-    end
-  end
 end
 
-module Clock
-  class Second
-    def initialize(digits)
-      @digits = digits
-    end
+class Baseball::Second
+  def initialize(player, uniform_number)
+    @player = player
+    @uniform_number = uniform_number
   end
 end
 
 p Baseball::Second.new('Alice', 13)
-p Clock::Second.new(13)
