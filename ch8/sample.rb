08e58d8a1904cb81f75f563b09b967a9a4631bba
yinm <yinm.cminor9@gmail.com>
2017-12-07 08:13:26 +0900

    Check module having state

--- ch8/sample.rb
+++ ch8/sample.rb
@@ -1,2 +1,28 @@
-Kernel.puts 'Hello.'
-Kernel.p [1, 2, 3]
+module AwesomeApi
+  @base_url = ''
+  @debug_mode = false
+
+  class << self
+    def base_url=(value)
+      @base_url = value
+    end
+
+    def base_url
+      @base_url
+    end
+
+    def debug_mode=(value)
+      @debug_mode = value
+    end
+
+    def debug_mode
+      @debug_mode
+    end
+  end
+end
+
+AwesomeApi.base_url = 'http://example.com'
+AwesomeApi.debug_mode = true
+
+p AwesomeApi.base_url
+p AwesomeApi.debug_mode
