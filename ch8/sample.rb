01b2beb66d46d4f44e432c35331e4b075e841798
yinm <yinm.cminor9@gmail.com>
2017-12-07 08:35:49 +0900

    Check prepend 2

--- ch8/sample.rb
+++ ch8/sample.rb
@@ -12,5 +12,18 @@ class Product
   end
 end
 
+class ProductIncluded
+  include A
+
+  def to_s
+    "<ProductIncluded> #{super}"
+  end
+end
+
 product = Product.new
 p product.to_s
+p Product.ancestors
+
+product_included = ProductIncluded.new
+p product_included.to_s
+p ProductIncluded.ancestors
