a9078b4bfb8b648502477b648e537b347c6c947a
yinm <yinm.cminor9@gmail.com>
2017-12-07 08:04:13 +0900

    Check constant in module

--- ch8/sample.rb
+++ ch8/sample.rb
@@ -1,33 +1,9 @@
 module Loggable
-  def bar(text)
-    puts "[BAR] #{text}"
-  end
-
-  module_function
+  PREFIX = '[LOG]'.freeze
 
   def log(text)
-    puts "[LOG] #{text}"
-  end
-
-  def foo(text)
-    puts "[FOO] #{text}"
+    puts "#{PREFIX} #{text}"
   end
 end
 
-Loggable.log 'hello.'
-Loggable.foo 'hello.'
-
-class Product
-  include Loggable
-
-  def title
-    bar 'hello.'
-    log 'hello.'
-    foo 'hello.'
-
-    'testだよ'
-  end
-end
-
-product = Product.new
-p product.title
+p Loggable::PREFIX
