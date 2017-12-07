f97d950a82e03f497931261875052c4878eea838
yinm <yinm.cminor9@gmail.com>
2017-12-08 08:43:10 +0900

    Check Exception object

--- ch9/sample.rb
+++ ch9/sample.rb
@@ -1,19 +1,9 @@
-def method_1
-  puts 'method_1 start.'
-  method_2
-  puts 'method_1 end.'
+begin
+  1 / 0
+rescue => e
+  puts "エラークラス: #{e.class}"
+  puts "エラーメッセージ: #{e.message}"
+  puts "バックトレース ------"
+  puts e.backtrace
+  puts "------"
 end
-
-def method_2
-  puts 'method_2 start.'
-  method_3
-  puts 'method_2 end.'
-end
-
-def method_3
-  puts 'method_3 start.'
-  1 / 0 # ZeroDivisionErrorを発生させる
-  puts 'method_3 end.'
-end
-
-method_1
