effc186aede81b5c399ec949051e215149a7375b
yinm <yinm.cminor9@gmail.com>
2017-12-09 19:37:46 +0900

    Check exception object variable

--- ch9/sample.rb
+++ ch9/sample.rb
@@ -1,5 +1,6 @@
 begin
   'abc'.foo
-rescue ZeroDivisionError, NoMethodError
+rescue ZeroDivisionError, NoMethodError => e
   puts '0で除算したか、存在しないメソッドが呼び出されました'
+  puts "エラー: #{e.class} #{e.message}"
 end
