7f3ba0500b43fabb2f822c27f93cfeeabf6976c3
yinm <yinm.cminor9@gmail.com>
2017-12-10 15:16:53 +0900

    Check original exception class 2

--- ch9/sample.rb
+++ ch9/sample.rb
@@ -1,4 +1,10 @@
 class NoCountryError < StandardError
+  attr_reader :country
+
+  def initialize(message, country)
+    @country = country
+    super("#{message} #{country}")
+  end
 end
 
 def currency_of(country)
@@ -10,8 +16,13 @@ def currency_of(country)
     when :india
       'rupee'
     else
-      raise NoCountryError, "無効な国名です。#{country}"
+      raise NoCountryError.new('無効な国名です。', country)
   end
 end
 
-currency_of(:italy)
+begin
+  currency_of(:italy)
+rescue NoCountryError => e
+  puts e.message
+  puts e.country
+end
