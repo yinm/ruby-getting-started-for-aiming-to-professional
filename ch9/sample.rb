211c93cbb7f52f747dd1caaed2014a5a9c4847f9
yinm <yinm.cminor9@gmail.com>
2017-12-10 14:53:43 +0900

    Check begin ~ end shortcut

--- ch9/sample.rb
+++ ch9/sample.rb
@@ -1,13 +1,17 @@
-begin
-  1 / 0
-rescue => e
-  puts "#{e.class} #{e.message}"
-  puts e.backtrace
+def fizz_buzz(n)
+  begin
+    if n % 15 == 0
+      'Fizz Buzz'
+    elsif n % 3 == 0
+      'Fizz'
+    elsif n % 5 == 0
+      'Buzz'
+    else
+      n.to_s
+    end
+  rescue => e
+    puts "#{e.class} #{e.message}"
+  end
 end
 
-begin
-  1 / 0
-rescue
-  puts "#{$!.class} #{$!.message}"
-  puts $@
-end
+fizz_buzz(nil)
