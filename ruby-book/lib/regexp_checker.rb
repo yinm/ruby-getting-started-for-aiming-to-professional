e423f95e30ed31400611b10704f8a7ec00b21b41
yinm <yinm.cminor9@gmail.com>
2017-12-10 14:23:36 +0900

    Add regexp

--- ruby-book/lib/regexp_checker.rb
+++ ruby-book/lib/regexp_checker.rb
@@ -1,3 +1,12 @@
 print 'Text?: '
 text = gets.chomp
-puts text
+print 'Pattern?: '
+pattern = gets.chomp
+
+regexp = Regexp.new(pattern)
+matches = text.scan(regexp)
+if matches.size > 0
+  puts "Matched: #{matches.join(', ')}"
+else
+  puts 'Nothing matched.'
+end
