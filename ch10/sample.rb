aed4058d08e5b6f2112b8f4bbf2b21e30976413f
yinm <yinm.cminor9@gmail.com>
2017-12-10 16:21:58 +0900

    Check arguments with Proc object

--- ch10/sample.rb
+++ ch10/sample.rb
@@ -1,9 +1,11 @@
-def greeting(arrange_proc)
-  puts 'おはよう'
-  text = arrange_proc.call('こんにちは')
-  puts text
-  puts 'こんばんは'
+def greeting(proc_1, proc_2, proc_3)
+  puts proc_1.call('おはよう')
+  puts proc_2.call('こんにちは')
+  puts proc_3.call('こんばんは')
 end
 
+shuffle_proc = Proc.new { |text| text.chars.shuffle.join }
 repeat_proc = Proc.new { |text| text * 2 }
-greeting(repeat_proc)
+question_proc = Proc.new { |text| "#{text}?" }
+
+greeting(shuffle_proc, repeat_proc, question_proc)
