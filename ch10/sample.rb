2b631b9b1de43a9f7da3d479e21b133d218936fc
yinm <yinm.cminor9@gmail.com>
2017-12-10 16:16:12 +0900

    Check Proc object 2

--- ch10/sample.rb
+++ ch10/sample.rb
@@ -1,12 +1,9 @@
 def greeting(&block)
-  puts block.class
-  
   puts 'おはよう'
   text = block.call('こんにちは')
   puts text
   puts 'こんばんは'
 end
 
-greeting do |text|
-  text * 2
-end
+repeat_proc = Proc.new { |text| text * 2 }
+greeting(&repeat_proc)
