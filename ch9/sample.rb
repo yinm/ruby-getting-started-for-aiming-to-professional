f5f8e6f77cb466b602abef65a2f29cd3a4392500
yinm <yinm.cminor9@gmail.com>
2017-12-10 14:42:47 +0900

    Check retrun value of exception 2

--- ch9/sample.rb
+++ ch9/sample.rb
@@ -1,22 +1,12 @@
-ret =
+def some_method(n)
   begin
-    'OK'
+    1 / n
   rescue
     'error'
   ensure
     'ensure'
   end
+end
 
-p ret
-
-ret =
-  begin
-    1 / 0
-    'OK'
-  rescue
-    'error'
-  ensure
-    'ensure'
-  end
-
-p ret
+p some_method(1)
+p some_method(0)
