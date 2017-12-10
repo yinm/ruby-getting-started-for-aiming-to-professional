88a268f936e467c8e31cf957bc442721337c0a05
yinm <yinm.cminor9@gmail.com>
2017-12-10 14:55:07 +0900

    Check begin ~ end shortcut 2

--- ch9/sample.rb
+++ ch9/sample.rb
@@ -1,17 +1,15 @@
 def fizz_buzz(n)
-  begin
-    if n % 15 == 0
-      'Fizz Buzz'
-    elsif n % 3 == 0
-      'Fizz'
-    elsif n % 5 == 0
-      'Buzz'
-    else
-      n.to_s
-    end
-  rescue => e
-    puts "#{e.class} #{e.message}"
+  if n % 15 == 0
+    'Fizz Buzz'
+  elsif n % 3 == 0
+    'Fizz'
+  elsif n % 5 == 0
+    'Buzz'
+  else
+    n.to_s
   end
+rescue => e
+  puts "#{e.class} #{e.message}"
 end
 
 fizz_buzz(nil)
