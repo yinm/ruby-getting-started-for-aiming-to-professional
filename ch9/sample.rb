87788e5d6ef655189177115073b1fa8b44fda8be
yinm <yinm.cminor9@gmail.com>
2017-12-10 14:16:16 +0900

    Check gets method

--- ch9/sample.rb
+++ ch9/sample.rb
@@ -1,14 +1,2 @@
-def currency_of(country)
-  case country
-    when :japan
-      'yen'
-    when :us
-      'dollar'
-    when :india
-      'rupee'
-    else
-      raise ArgumentError, "無効な国名です。 #{country}"
-  end
-end
-
-currency_of(:italy)
+input = gets
+p input
