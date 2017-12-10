26ae2761df58de71f1fbd93c292df33dc86bfd72
yinm <yinm.cminor9@gmail.com>
2017-12-10 16:05:26 +0900

    Check Proc object

--- ch10/sample.rb
+++ ch10/sample.rb
@@ -1,21 +1,7 @@
-def greeting(&block)
-  puts 'おはよう'
-  
-  text =
-    if block.arity == 1
-      yield 'こんにちは'
-    elsif block.arity == 2
-      yield 'こんに', 'ちは'
-    end
-  puts text
-  
-  puts 'こんばんは'
+hello_proc = Proc.new do
+  'Hello!'
 end
+p hello_proc
 
-greeting do |text|
-  text * 2
-end
-
-greeting do |text_1, text_2|
-  text_1 * 2 + text_2 * 2
-end
+hello_proc = Proc.new { 'Hello!' }
+p hello_proc
