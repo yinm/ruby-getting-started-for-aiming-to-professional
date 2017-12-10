fc0400732fa71e9883251a7eb27348576c74107c
yinm <yinm.cminor9@gmail.com>
2017-12-10 16:33:05 +0900

    Check lambda? method

--- ch10/sample.rb
+++ ch10/sample.rb
@@ -1,8 +1,7 @@
-add_proc = Proc.new { |a, b| a.to_i + b.to_i }
-p add_proc.call(10)
-p add_proc.call(10, 20, 100)
+add_proc = Proc.new { |a, b| a + b }
+p add_proc.class
+p add_proc.lambda?
 
-add_lambda = ->(a, b) { a.to_i + b.to_i }
-# p add_lambda.call(10)
-p add_lambda.call(10, 20)
-p add_lambda.call(10, 20, 30)
+add_lambda = ->(a, b) { a + b }
+p add_lambda.class
+p add_lambda.lambda?
