17378cad9113ab1df5ea81cc79ddffbc3d3f6923
yinm <yinm.cminor9@gmail.com>
2017-12-10 14:39:41 +0900

    Check else

--- ch9/sample.rb
+++ ch9/sample.rb
@@ -1,3 +1,14 @@
-File.open('some2.txt', 'w') do |file|
-  file << 'Hello'
+begin
+  puts 'Hello.'
+rescue
+  puts '例外が発生しました。'
+else
+  puts '例外は発生しませんでした。'
+end
+
+begin
+  puts 'Hello.'
+  puts '例外は発生しませんでした。'
+rescue
+  puts '例外が発生しました'
 end
