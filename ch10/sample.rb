6dc51cd82edee184ad7c1efb502a0073b76328df
yinm <yinm.cminor9@gmail.com>
2017-12-10 16:25:26 +0900

    Check lambda

--- ch10/sample.rb
+++ ch10/sample.rb
@@ -1,11 +1,5 @@
-def greeting(proc_1, proc_2, proc_3)
-  puts proc_1.call('おはよう')
-  puts proc_2.call('こんにちは')
-  puts proc_3.call('こんばんは')
-end
+add_proc = Proc.new { |a, b| a + b }
+p add_proc.call(10, 20)
 
-shuffle_proc = Proc.new { |text| text.chars.shuffle.join }
-repeat_proc = Proc.new { |text| text * 2 }
-question_proc = Proc.new { |text| "#{text}?" }
-
-greeting(shuffle_proc, repeat_proc, question_proc)
+add_lambda = ->(a, b) { a + b }
+p add_lambda.call(10, 20)
