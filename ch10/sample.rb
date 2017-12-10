5058c4c1f398a85355956c7e8f0c7512cd1e13c3
yinm <yinm.cminor9@gmail.com>
2017-12-10 15:49:50 +0900

    Check block

--- ch10/sample.rb
+++ ch10/sample.rb
@@ -1,9 +1,13 @@
 def greeting(&block)
   puts 'おはよう'
-  text = block.call('こんにちは')
-  puts text
+  unless block.nil?
+    text = block.call('こんにちは')
+    puts text
+  end
   puts 'こんばんは'
 end
+
+greeting
 
 greeting do |text|
   text * 2
