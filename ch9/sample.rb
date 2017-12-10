cd83e6469b11123fc23d388b379f3c6825ca1d9e
yinm <yinm.cminor9@gmail.com>
2017-12-10 13:26:58 +0900

    Check case statement

--- ch9/sample.rb
+++ ch9/sample.rb
@@ -1,16 +1,12 @@
-require 'date'
-
-def convert_heisei_to_date(heisei_text)
-  m = heisei_text.match(/平成(?<jp_year>\d+)年(?<month>\d+)月(?<day>\d+)日/)
-
-  year = m[:jp_year].to_i + 1988
-  month = m[:month].to_i
-  day = m[:day].to_i
-
-  if Date.valid_date?(year, month, day)
-    Date.new(year, month, day)
+def currency_of(country)
+  case country
+    when :japan
+      'yen'
+    when :us
+      'dollar'
+    when :india
+      'rupee'
   end
 end
 
-p convert_heisei_to_date('平成28年12月31日')
-p convert_heisei_to_date('平成28年99月99日')
+p currency_of(:italy)
