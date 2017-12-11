c556912fa7561ad2148ee9d629ea2fb341a113e1
yinm <yinm@pepabo.com>
2017-12-11 09:47:39 +0900

    Check === operator with Proc object

--- ch10/sample.rb
+++ ch10/sample.rb
@@ -1,3 +1,17 @@
-add_proc = Proc.new { |a, b| a + b }
+def judge(age)
+  adult = Proc.new { |n| n > 20 }
+  child = Proc.new { |n| n < 20 }
 
-p add_proc === [10, 20]
+  case age
+    when adult
+      '大人です'
+    when child
+      '子供です'
+    else
+      'はたちです'
+  end
+end
+
+p judge(25)
+p judge(18)
+p judge(20)
