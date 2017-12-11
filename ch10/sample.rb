fa81ecd0d28696ffd12688b5a2b2f41419f663f5
yinm <yinm.cminor9@gmail.com>
2017-12-12 08:11:51 +0900

    Check difference between Proc and lambda 2

--- ch10/sample.rb
+++ ch10/sample.rb
@@ -1,14 +1,14 @@
-def proc_return
-  f = Proc.new { |n| return n * 10 }
+def proc_break
+  f = Proc.new { |n| break n * 10 }
   ret = [1, 2, 3].map(&f)
   "ret: #{ret}"
 end
 
-def lambda_return
-  f = ->(n) { return n * 10 }
+def lambda_break
+  f = ->(n) { break n * 10 }
   ret = [1, 2, 3].map(&f)
   "ret: #{ret}"
 end
 
-p proc_return
-p lambda_return
+# p proc_break
+p lambda_break
