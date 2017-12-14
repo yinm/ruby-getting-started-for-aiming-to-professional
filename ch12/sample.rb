8895387dd1ca9ca17e67086349da31b70b3a9d05
yinm <yinm@pepabo.com>
2017-12-14 09:43:38 +0900

    Check csv library

--- ch12/sample.rb
+++ ch12/sample.rb
@@ -1,8 +1,10 @@
-require 'pathname'
+require 'csv'
 
-lib = Pathname.new('../ruby-book/lib')
+CSV.open('./sample.csv', 'w') do |csv|
+  csv << ['Name', 'Email', 'Age']
+  csv << ['Alice', 'alice@example.com', 20]
+end
 
-p lib.file?
-p lib.directory?
-
-p lib.join('sample.txt').to_s
+CSV.foreach('./sample.csv') do |row|
+  puts "1: #{row[0]}, 2: #{row[1]}, 3: #{row[2]}"
+end
